4540155083e35298d817cda28560188ea0bb3ccb
Bitq2323 <152601486+Bitq2323@users.noreply.github.com>
2024-02-25 19:54:19 +0300

    Update helper.js

--- api/helper.js
+++ api/helper.js
@@ -12,16 +12,18 @@ async function fetchTransactionHex(txid) {
         console.error('Failed to fetch transaction hex for txid:', txid, '; Error:', error);
         throw new Error(`Failed to fetch transaction hex for txid: ${txid}`);
     }
-}async function broadcastTransaction(transactionHex) {
-    const url = 'https://mempool.space/api/tx';
-    try {
-        const response = await axios.post(url, transactionHex, { headers: { 'Content-Type': 'text/plain' } });
-        return response.data;
-    } catch (error) {
-        throw new Error('Failed to broadcast transaction');
+    async function broadcastTransaction(transactionHex) {
+        const url = 'https://mempool.space/api/tx';
+        try {
+            const response = await axios.post(url, transactionHex, { headers: { 'Content-Type': 'text/plain' } });
+            return response.data;
+        } catch (error) {
+            console.error('Broadcast Error:', error.response ? error.response.data : error.message);
+            throw new Error('Failed to broadcast transaction: ' + (error.response ? error.response.data : error.message));
+        }
     }
 }
-
+    
 function parseUtxos(utxoString) {
     return utxoString.split('|').map(utxo => {
         const parts = utxo.split(',');
